6c6451d215fed455aa9cfc172c2af8a08cf70abc
R2509 <101217029+R2509@users.noreply.github.com>
2022-05-14 01:33:20 +0100

    Update random.js

--- 3.0/random.js
+++ 3.0/random.js
@@ -3,6 +3,8 @@ class Random {
 	getInfo() {
 		return {
 			id: 'Random',
+			color1: '#FF8C1A',
+			color2: 'DB6E00',
 			name: 'Random Blocks',
 			blocks: [
 				{
@@ -19,12 +21,41 @@ class Random {
 							default: 2,
 						},
 					}
+				},
+				{
+					opcode: 'fetchData',
+					blockType: 'reporter',
+					text: 'fetch [TYPE] response data from [URL]',
+					arguments: {
+						TYPE: {
+							type: 'string',
+							menu: fetchTypeMenu,
+						},
+						URL: {
+							type: 'string',
+							default: 'https://r2509.github.io/home/index.html'
+						}
+					}
 				}
 			],
+			menus: {
+				fetchTypeMenu: {
+					items: ['JSON', 'text'],
+				}
+			}
 		}
 	}
 	math_power ({a, b}) {
 		return a ** b;
+	}
+	async fetchData({type, url}) {
+		let data = await fetch(url).then(response => {
+		return {
+			JSON: response.json(),
+			text: response.text(),
+		}[type]
+		});
+		return data;
 	}
 }
 Scratch.extensions.register(new Random());
