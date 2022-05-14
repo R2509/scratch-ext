ebffaf4d5ebd743d09d7cb0facb32bd25083e129
R2509 <101217029+R2509@users.noreply.github.com>
2022-05-14 18:36:59 +0100

    Update random.js

--- 3.0/random.js
+++ 3.0/random.js
@@ -2,51 +2,44 @@ class Random {
 	constructor() {}
 	getInfo() {
 		return {
-			id: 'Random',
-			name: 'Random Blocks',
-			blocks: [
+			"id": "Random",
+			"name": "Random Blocks",
+			"blocks": [
 				{
-					opcode: 'math_power',
-					blockType: 'reporter',
-					text: '[A] ^ [B]',
-					arguments: {
-						A: {
-							type: 'number',
-							default: 5,
+					"opcode": "math_power",
+					"blockType": "reporter",
+					"text": "[num1] ^ [num2]",
+					"arguments": {
+						"num1": {
+							"type": "number",
+							"defaultValue": 5,
 						},
-						B: {
-							type: 'number',
-							default: 2,
+						"num2": {
+							"type": "number",
+							"defaultValue": 2,
 						},
 					}
 				},
 				{
-					opcode: 'fetchData',
-					blockType: 'reporter',
-					text: 'fetch [TYPE] response data from [URL]',
-					arguments: {
-						TYPE: {
-							type: 'string',
-							menu: 'fetchTypeMenu',
-						},
-						URL: {
-							type: 'string',
-							defaultValue: 'https://r2509.github.io/home/index.html'
+					"opcode": "fetchDat",
+					"blockType": "reporter",
+					"text": "fetch response data from [url]",
+					"arguments": {
+						"url": {
+							"type": "string",
+							"defaultValue": "https://r2509.github.io/home/index.html",
 						}
 					}
 				}
 			],
-			menus: {
-				fetchTypeMenu: {
-					items: ['JSON', 'text'],
-				}
+			"menus": {
 			}
 		}
 	}
 	math_power ({a, b}) {
 		return a ** b;
 	}
-	fetchData({type, url}) {
+	fetchDat({url}) {
 		return fetch(url).then(response => response.text());
 	}
 }
