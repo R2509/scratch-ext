f72ba8b4a36798dc905b7f1bc9ea053c2395183b
R2509 <101217029+R2509@users.noreply.github.com>
2022-05-25 02:32:24 +0100

    Update storage.js

--- 3.0/storage.js
+++ 3.0/storage.js
@@ -6,18 +6,43 @@ class LStorage {
 		return {
 			id: 'storage',
 			name: 'Storage',
-			blocks: [{
-				opcode: 'getFromLS',
-				blockType: 'reporter',
-				text: 'get [key] from localStorage',
-				arguments: {
-					key: {
-						type: 'string',
+			blocks: [
+				{
+					opcode: 'getFromLS',
+					blockType: 'reporter',
+					text: 'get [key] from localStorage',
+					arguments: {
+						key: {
+							type: 'string',
+							defaultValue: 'foo'
+						}
+					}
+				},
+				{
+					opcode: 'addToLS',
+					blockType: 'command',
+					text: 'set [key] of localStorage to [value]',
+					arguments: {
+						key: {
+							type: 'string',
+							defaultValue: 'foo'
+						},
+						value: {
+							type: 'string',
+							defaultValue: 'bar'
+						}
 					}
 				}
-			}]
+			]
 		}
 	};
+	newLS() {
+		localStorage.Scratch = {};
+		localStorage.Scratch.LS = {
+			version: '0.1.0',
+			data: {},
+		}
+	}
 	getFromLS({key}) {
 		if (localStorage.Scratch.LS.data) {
 			if (localStorage.Scratch.LS.data[key]) {
@@ -26,12 +51,14 @@ class LStorage {
 				return '';
 			}
 		} else {
-			localStorage.Scratch = {};
-			localStorage.Scratch.LS = {
-				version: '0.1.0',
-				data: {},
-			};
+			newLS()
 		}
 	};
+	addToLS({key, value}) {
+		if (localStorage.Scratch.LS.data) {
+			localStroage.Scratch.LS.data[key] = value;
+		} else {
+			newLS()
+	}
 }
 Scratch.extensions.register(new LStorage());
