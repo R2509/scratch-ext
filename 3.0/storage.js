cc9ac96d821346eb62702f68254be9f4182b6ac7
R2509 <101217029+R2509@users.noreply.github.com>
2022-05-25 02:09:18 +0100

    Update storage.js

--- 3.0/storage.js
+++ 3.0/storage.js
@@ -4,24 +4,21 @@ class LStorage {
 	}
 	getInfo() {
 		return {
-	id: 'storage',
-	name: 'Storage',
-	blocks: [
-		{
-			opcode: 'getFromLS',
-			blockType: 'reporter',
-			text: 'get [key] from localStorage',
-			arguments: {
-				key: {
-					type: 'string',
-					menu: 'dynamicMenu'
+			id: 'storage',
+			name: 'Storage',
+			blocks: [{
+				opcode: 'getFromLS',
+				blockType: 'reporter',
+				text: 'get [key] from localStorage',
+				arguments: {
+					key: {
+						type: 'string',
+					}
 				}
-			}
+			}]
 		}
-	]
-}
 	};
-	getFromLS ({key}) {
+	getFromLS({key}) {
 		if (localStorage.Scratch.LS.data) {
 			if (localStorage.Scratch.LS.data[key]) {
 				return localStorage.Scratch.LS.data[key];
