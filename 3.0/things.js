dbe852b976c30ede2372a77e96cae0eb1e38b305
R2509 <101217029+R2509@users.noreply.github.com>
2022-05-30 10:42:33 +0100

    Create things.js

---
+++ 3.0/things.js
@@ -0,0 +1,36 @@
+class Things {
+	constructor () {}
+	getInfo () {
+		return {
+			id: 'Things',
+			name: 'Things',
+			blocks: [
+				{
+					opcode: 'substring',
+					blockType: 'reporter',
+					text: 'substring of [str] from [i1] to [i2]',
+					arguments: {
+						str: {
+							type: 'string',
+							defaultValue: 'Hello world!'
+						},
+						i1: {
+							type: 'number',
+							defaultValue: 7
+						},
+						i2: {
+							type: 'number',
+							defaultValue: 13
+						}
+					},
+				},
+			]
+		}
+	}
+	
+	substring ({str, i1, i2}) {
+		return str.substring(i1+1, i2+2);
+	}
+}
+
+Scrath.extensions.register(new Things())
